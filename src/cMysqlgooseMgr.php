63a36787dded5d387952f25734bee7f042e260f2
aponica-release <github@aponica.com>
2022-09-08 12:09:24 -0400

    Supports passing definitions (via goosemgr-php v2) and config filenames.

--- src/cMysqlgooseMgr.php
+++ src/cMysqlgooseMgr.php
@@ -22,15 +22,21 @@ class cMysqlgooseMgr extends cGooseMgr {
   //---------------------------------------------------------------------------
   /// Constructs a cMysqlgooseMgr object.
   ///
-  /// @param hDefinitions
+  /// @param $vDefinitions
   ///   The definitions hash (associative array) used to create schemas
   ///   for the database.
+  ///
+  ///   If `$vDefinitions` is a string, it is assumed to be the filename
+  ///   (in the <a
+  ///   href="https://www.php.net/manual/en/ini.core.php#ini.include-path">
+  ///   include path</a>) from which the hash can be read as JSON objects
+  ///   (JSON uses objects instead of associative arrays).
   ///
   ///   The hash contains one member for each table or collection to
   ///   be accessed. The name of the member is the name of the table or
   ///   collection, and its value is a hash as expected by
   ///   <a href="https://aponica.com/docs/mysqlgoose-php/classAponica_1_1Mysqlgoose_1_1Schema.html"
-  ///   >Aponica\Mysqlgoose\Schema</a>.
+  ///   >\Aponica\Mysqlgoose\Schema</a>.
   ///
   ///   The hash may contain a member named `"//"` with any value;
   ///   it is assumed to be a comment member, and is ignored.
@@ -40,8 +46,8 @@ class cMysqlgooseMgr extends cGooseMgr {
   ///     >npx @aponica/mysqlgoose-schema-js</a> using `json_decode()`.
   //---------------------------------------------------------------------------
 
-  function __construct( $hDefinitions ) {
-    parent::__construct( $hDefinitions, new Mysqlgoose() );
+  function __construct( mixed $vDefinitions ) {
+    parent::__construct( $vDefinitions, new Mysqlgoose() );
     }
 
 
@@ -53,20 +59,29 @@ class cMysqlgooseMgr extends cGooseMgr {
   /// >\Aponica\GooseMgr\cGooseMgr::fConnect()</a> to establish the
   /// connection.
   ///
-  /// @param hConfig
+  /// @param $vConfig
   ///   The configuration parameters as expected by <a
   ///   href="https://aponica.com/docs/mysqlgoose-php/classAponica_1_1Mysqlgoose_1_1Mysqlgoose.html"
   ///   >\Aponica\Mysqlgoose\Mysqlgoose::connect()</a>.
+  ///
+  ///   If `$vConfig` is a string, it is assumed to be the filename
+  ///   (in the <a
+  ///   href="https://www.php.net/manual/en/ini.core.php#ini.include-path">
+  ///   include path</a>) supplying the configuration data in JSON format.
   ///
   /// @returns Mysqlgoose:
   ///   A connection to the database (really, a Mysqlgoose instance).
   //---------------------------------------------------------------------------
 
-  protected function fiCreateConnection( $hConfig ) : object {
+  protected function fiCreateConnection( mixed $vConfig ) : object {
 
     // TODO: support multiple connections
 
-    $this->fiGoose()->connect( $hConfig );
+    if ( "string" === gettype( $vConfig ) )
+      $vConfig = json_decode(
+        file_get_contents( $vConfig, true ), true );
+
+    $this->fiGoose()->connect( $vConfig );
     return $this->fiGoose();
 
     } // fiCreateConnection
